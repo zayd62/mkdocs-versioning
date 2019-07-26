066acd793fb1da4fff7350dbeacd53a67082ff7f
Zayd Patel <zayd62@gmail.com>
2019-07-26 17:00:13 +0100

    WIP: added handling for version
    checks and extracts the version number. handles when version number not
    available

--- mkversion/version.py
+++ mkversion/version.py
@@ -10,3 +10,13 @@ class Version(BasePlugin):
         ('rebuild', config_options.Type(bool, default=False)),
     )
 
+    def on_config(self, config, **kwargs):
+
+        # extract the version number
+        try:
+            version = config['extra']['version']
+        except KeyError as e:
+            print(e)
+            print('Warning: ' +
+                  'no version detected in mkdocs.yml.You should specify a version number (ideally) according to semantic versioning in mkdocs.yml. exiting')
+            sys.exit(1)
