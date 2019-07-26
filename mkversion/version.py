4fd6528c0c250d886e1a657b2925b2009fd1db0c
Zayd Patel <zayd62@gmail.com>
2019-07-26 17:04:17 +0100

    WIP: generating new build directory name

--- mkversion/version.py
+++ mkversion/version.py
@@ -24,3 +24,7 @@ class Version(BasePlugin):
         # changing the site name to include the verison number
         config['site_name'] = config['site_name'] + ' - ' + version
         print('the new site_name:  ', config['site_name'])
+
+        # creating new directory from site_dir and version number
+        new_dir = os.path.join(config['site_dir'], config['extra']['version'])
+        print("the new build directory is", new_dir)
