e4be17b3d3cb7476085034c305ba9a6c72352342
Zayd Patel <zayd62@gmail.com>
2019-07-26 16:50:33 +0100

    chore: updated cz-config.js

--- .cz-config.js
+++ .cz-config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  types: [
+    { value: 'feat',     name: 'A new feature'},
+    { value: 'fix',      name: 'A bug fix'},
+    { value: 'docs',     name: 'Documentation only changes'},
+    { value: 'style',    name: 'Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)'},
+    { value: 'refactor', name: 'A code change that neither fixes a bug nor adds a feature'},
+    { value: 'perf',     name: 'A code change that improves performance'},
+    { value: 'test',     name: 'Adding missing tests or correcting existing tests'},
+    { value: 'build',    name: 'Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)'},
+    { value: 'ci',       name: 'Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)'},
+    { value: 'chore',    name: 'Other changes that don\'t modify src or test files'}
+    { value: 'WIP',      name: 'Work in progress' },
+    ],
+
   scopes: [{ name: 'accounts' }, { name: 'admin' }, { name: 'exampleScope' }, { name: 'changeMe' }],
   allowCustomScopes: true,
 };
