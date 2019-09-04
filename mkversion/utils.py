8f9033468bf407054d6bad4a06c9c9fc4bd7cd16
Zayd Patel <zayd62@gmail.com>
2019-09-04 13:34:25 +0100

    feat: added sync command

--- mkversion/utils.py
+++ mkversion/utils.py
@@ -1,11 +1,12 @@
-import argparse
-from pprint import pprint
-
+import subprocess
+import tempfile
+import os
+import shutil
 from mkdocs.commands import gh_deploy
 from mkdocs import config, exceptions
 
 
-def deploy(args: argparse.Namespace):
+def deploy(args):
     """
     Deploy to Github Pages
 
@@ -15,7 +16,6 @@ def deploy(args: argparse.Namespace):
     Raises:
         exceptions.ConfigurationError
     """
-    print('vars(args) -->', vars(args))
 
     try:
         cfg = config.load_config(config_file=args.config_file, remote_branch=args.remote_branch, remote_name=args.remote_name)
@@ -24,3 +24,34 @@ def deploy(args: argparse.Namespace):
     except exceptions.ConfigurationError as e:
         raise SystemExit('\n' + str(e))
 
+
+def sync(args):
+    """
+    Pulls the previously built pages from github pages
+
+    Args:
+        args (argparse.Namespace): A Namespace object contaning all the command line arguments
+
+    Raises:
+        exceptions.ConfigurationError
+    """
+
+    cfg = config.load_config(config_file=args.config_file)
+    with tempfile.TemporaryDirectory() as tempdir:
+        # clone gh-pages branch into a temp dir
+        os.chdir(tempdir)
+        subprocess.run(['git', 'clone', '-b', cfg['remote_branch'], cfg['repo_url']])
+        os.chdir(os.listdir()[0])
+
+        # remove old site folder
+        try:
+            shutil.rmtree(cfg['site_dir'])
+            os.mkdir(cfg['site_dir'])  # rmtree deletes folder so need to recreate folder
+        except FileNotFoundError as identifier:
+            print("no site directory")
+
+        # copy files into site directory
+        with os.scandir(os.getcwd()) as files:
+            for i in files:
+                print(i.name, i.path)
+                shutil.move(i.path, cfg['site_dir'])
