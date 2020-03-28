6d6ddd45f4667178271d4de5261e473052109eef
Zayd <zayd62@gmail.com>
2020-03-28 21:20:19 +0000

    build: updated version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mkdocs-versioning",
-    version="0.2.0",
+    version="0.2.1",
     author="Zayd Patel",
     author_email="zayd62@gmail.com",
     description="A tool that allows for versioning sites built with mkdocs",
