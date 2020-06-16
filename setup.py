0adf84cbb7eac97cd330f380c03aafadd32c65d2
Zayd Patel <zayd62@gmail.com>
2020-06-16 16:30:46 +0100

    build: update setup.py with updated python version and classifiers
    build: update setup.py with updated python version and classifiers

--- setup.py
+++ setup.py
@@ -20,9 +20,6 @@ setuptools.setup(
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         "Programming Language :: Python :: Implementation :: CPython",
@@ -36,7 +33,7 @@ setuptools.setup(
             'mkdocs-versioning = mkversion.entry:Entry',
         ]
     },
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     project_urls={
         'Documentation': 'https://zayd62.github.io/mkdocs-versioning/'
     },
