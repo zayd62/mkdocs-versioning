b66ae1aa620daaf3d3b40878ba43865a08c385da
neatc0der <grosch.tobias@googlemail.com>
2021-01-26 23:48:33 +0100

    repair typo

--- mkversion/entry.py
+++ mkversion/entry.py
@@ -44,7 +44,7 @@ class Entry(BasePlugin):
         if Entry.is_serving(config['site_dir']):
             nav = config['nav']
             for count, i in enumerate(nav):
-                if hasattr(i, 'keys') in [j.lower() for j in i.keys()]:
+                if hasattr(i, 'keys') and [j.lower() for j in i.keys()]:
                     del nav[count]
                     break
         # check if version selector is in nav
